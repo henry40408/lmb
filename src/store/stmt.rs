bb3e9cc819fc7df02b539c1272d0be4d3e550782
Heng-Yi Wu <2316687+henry40408@users.noreply.github.com>
2025-03-09 00:24:44 +0800

    refactor: query builder (#288)
    * refactor: query builder
    
    * chore: ignore unmaintained paste
    
    * chore: update ring to address vulnerability

--- src/store/stmt.rs
+++ src/store/stmt.rs
@@ -1,12 +1,89 @@
-pub(crate) const SQL_DELETE_VALUE_BY_NAME: &str = "DELETE FROM store WHERE name = ?1";
+use crate::Result;
+use sea_query::{Expr, Iden, OnConflict, Order, Query, SqliteQueryBuilder};
+use sea_query_rusqlite::{RusqliteBinder, RusqliteValues};
 
-pub(crate) const SQL_GET_ALL_VALUES: &str = "
-    SELECT name, size, type_hint, created_at, updated_at FROM store
-";
+#[derive(Iden)]
+#[iden(rename = "store")]
+pub(crate) enum StoreId {
+    Table,
+    Id,
+    Name,
+    Value,
+    Size,
+    TypeHint,
+    CreatedAt,
+    UpdatedAt,
+}
 
-pub(crate) const SQL_GET_VALUE_BY_NAME: &str = "SELECT value, type_hint FROM store WHERE name = ?1";
+pub(crate) fn stmt_delete_value_by_name<S>(name: S) -> (Box<str>, RusqliteValues)
+where
+    S: AsRef<str>,
+{
+    let (sql, values) = Query::delete()
+        .from_table(StoreId::Table)
+        .cond_where(Expr::column(StoreId::Name).eq(name.as_ref()))
+        .build_rusqlite(SqliteQueryBuilder);
+    (sql.into_boxed_str(), values)
+}
 
-pub(crate) const SQL_UPSERT_STORE: &str = r#"
-    INSERT INTO store (name, value, size, type_hint) VALUES (?1, ?2, ?3, ?4)
-    ON CONFLICT(name) DO UPDATE SET value = ?2, size = ?3, type_hint = ?4, updated_at = CURRENT_TIMESTAMP
-"#;
+pub(crate) fn stmt_get_all_values() -> (Box<str>, RusqliteValues) {
+    let (sql, values) = Query::select()
+        .columns([
+            StoreId::Name,
+            StoreId::Size,
+            StoreId::TypeHint,
+            StoreId::CreatedAt,
+            StoreId::UpdatedAt,
+        ])
+        .from(StoreId::Table)
+        .order_by(StoreId::Id, Order::Desc)
+        .build_rusqlite(SqliteQueryBuilder);
+    (sql.into_boxed_str(), values)
+}
+
+pub(crate) fn stmt_get_value_by_name<S>(name: S) -> (Box<str>, RusqliteValues)
+where
+    S: AsRef<str>,
+{
+    let (sql, values) = Query::select()
+        .columns([StoreId::Value, StoreId::TypeHint])
+        .from(StoreId::Table)
+        .cond_where(Expr::column(StoreId::Name).eq(name.as_ref()))
+        .build_rusqlite(SqliteQueryBuilder);
+    (sql.into_boxed_str(), values)
+}
+
+pub(crate) fn stmt_upsert_store<S, T>(
+    name: S,
+    value: Vec<u8>,
+    size: usize,
+    type_hint: T,
+) -> Result<(Box<str>, RusqliteValues)>
+where
+    S: AsRef<str>,
+    T: AsRef<str>,
+{
+    let size = u64::try_from(size)?;
+    let (sql, values) = Query::insert()
+        .into_table(StoreId::Table)
+        .columns([
+            StoreId::Name,
+            StoreId::Value,
+            StoreId::Size,
+            StoreId::TypeHint,
+        ])
+        .values_panic([
+            name.as_ref().into(),
+            value.into(),
+            size.into(),
+            type_hint.as_ref().into(),
+        ])
+        .on_conflict(
+            OnConflict::column(StoreId::Name)
+                .value(StoreId::UpdatedAt, Expr::current_timestamp())
+                .update_columns([StoreId::Value, StoreId::Size, StoreId::TypeHint])
+                .to_owned(),
+        )
+        .build_rusqlite(SqliteQueryBuilder);
+    Ok((sql.into_boxed_str(), values))
+}
