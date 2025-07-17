02d7b1a400707a0cf8b1f2db16aee740b5bad4fc
Heng-Yi Wu <2316687+henry40408@users.noreply.github.com>
2025-07-18 01:21:23 +0800

    feat: migrate benchmarks from bencher to criterion for improved performance analysis

--- benches/bench.rs
+++ benches/bench.rs
@@ -1,6 +1,6 @@
 use std::io::{BufReader, Cursor, Read as _};
 
-use bencher::{Bencher, benchmark_group, benchmark_main};
+use criterion::{BatchSize, Criterion, criterion_group, criterion_main};
 use lmb::{Evaluation, Store};
 use mlua::Lua;
 use serde_json::json;
@@ -8,58 +8,81 @@ use tokio::io::empty;
 
 static SCRIPT: &str = "return true";
 
-fn mlua_call(bencher: &mut Bencher) {
+fn mlua_call(c: &mut Criterion) {
     let vm = Lua::new();
-    vm.sandbox(true).unwrap();
     let f = vm.load(SCRIPT).into_function().unwrap();
-    bencher.iter(|| f.call::<bool>(()).unwrap());
-}
-
-fn mlua_eval(bencher: &mut Bencher) {
-    let vm = Lua::new();
-    bencher.iter(|| vm.load(SCRIPT).eval::<bool>());
-}
-
-fn mlua_sandbox_eval(bencher: &mut Bencher) {
-    let vm = Lua::new();
-    vm.sandbox(true).unwrap();
-    bencher.iter(|| vm.load(SCRIPT).eval::<bool>());
-}
-
-fn lmb_evaluate(bencher: &mut Bencher) {
-    let e = Evaluation::builder(SCRIPT, empty()).build().unwrap();
-    bencher.iter(|| e.evaluate().call().unwrap());
-}
-fn lmb_no_store(bencher: &mut Bencher) {
-    let e = Evaluation::builder(SCRIPT, empty()).build().unwrap();
-    bencher.iter(|| e.evaluate().call().unwrap());
-}
-
-fn store_update(bencher: &mut Bencher) {
-    let store = Store::default();
-    store.put("a", &json!(0)).unwrap();
-    bencher.iter(|| {
-        store.update(
-            &["a"],
-            |old| {
-                old.insert("a".into(), json!(1));
-                Ok(())
-            },
-            None,
-        )
+    c.bench_function("mlua call", |b| {
+        b.iter(|| f.call::<bool>(()).unwrap());
     });
 }
 
-fn lmb_default_store(bencher: &mut Bencher) {
+fn mlua_eval(c: &mut Criterion) {
+    let vm = Lua::new();
+    c.bench_function("mlua eval", |b| {
+        b.iter(|| vm.load(SCRIPT).eval::<bool>());
+    });
+}
+
+fn mlua_sandbox_call(c: &mut Criterion) {
+    let vm = Lua::new();
+    vm.sandbox(true).unwrap();
+    let f = vm.load(SCRIPT).into_function().unwrap();
+    c.bench_function("mlua sandbox call", |b| {
+        b.iter(|| f.call::<bool>(()).unwrap());
+    });
+}
+
+fn mlua_sandbox_eval(c: &mut Criterion) {
+    let vm = Lua::new();
+    vm.sandbox(true).unwrap();
+    c.bench_function("mlua sandbox eval", |b| {
+        b.iter(|| vm.load(SCRIPT).eval::<bool>());
+    });
+}
+
+fn lmb_evaluate(c: &mut Criterion) {
+    let e = Evaluation::builder(SCRIPT, empty()).build().unwrap();
+    c.bench_function("lmb evaluate", |b| {
+        b.iter(|| e.evaluate().call().unwrap());
+    });
+}
+fn lmb_no_store(c: &mut Criterion) {
+    let e = Evaluation::builder(SCRIPT, empty()).build().unwrap();
+    c.bench_function("lmb no store", |b| {
+        b.iter(|| e.evaluate().call().unwrap());
+    });
+}
+
+fn store_update(c: &mut Criterion) {
+    let store = Store::default();
+    store.put("a", &json!(0)).unwrap();
+
+    c.bench_function("store update", |b| {
+        b.iter(|| {
+            store.update(
+                &["a"],
+                |old| {
+                    old.insert("a".into(), json!(1));
+                    Ok(())
+                },
+                None,
+            )
+        });
+    });
+}
+
+fn lmb_default_store(c: &mut Criterion) {
     let store = Store::default();
     let e = Evaluation::builder(SCRIPT, empty())
         .store(store)
         .build()
         .unwrap();
-    bencher.iter(|| e.evaluate().call().unwrap());
+    c.bench_function("lmb default store", |b| {
+        b.iter(|| e.evaluate().call().unwrap());
+    });
 }
 
-fn lmb_update(bencher: &mut Bencher) {
+fn lmb_update(c: &mut Criterion) {
     let script = r#"
         return require("@lmb").store:update({ "a" }, function(s)
           s.a = s.a + 1
@@ -70,65 +93,93 @@ fn lmb_update(bencher: &mut Bencher) {
         .store(store)
         .build()
         .unwrap();
-    bencher.iter(|| e.evaluate().call().unwrap());
+    c.bench_function("lmb update", |b| {
+        b.iter(|| e.evaluate().call().unwrap());
+    });
 }
 
-fn lmb_read_all(bencher: &mut Bencher) {
+fn lmb_read_all(c: &mut Criterion) {
     let input = Cursor::new("0");
     let script = "return io.read('*a')";
     let e = Evaluation::builder(script, input).build().unwrap();
-    bencher.iter(|| {
-        let _ = e.rewind_input();
-        e.evaluate().call().unwrap()
+    c.bench_function("lmb read all", |b| {
+        b.iter_batched(
+            || {
+                let _ = e.rewind_input();
+            },
+            |_| e.evaluate().call().unwrap(),
+            BatchSize::SmallInput,
+        );
     });
 }
 
-fn lmb_read_line(bencher: &mut Bencher) {
+fn lmb_read_line(c: &mut Criterion) {
     let input = Cursor::new("0");
     let script = "return io.read('*l')";
     let e = Evaluation::builder(script, input).build().unwrap();
-    bencher.iter(|| {
-        let _ = e.rewind_input();
-        e.evaluate().call().unwrap()
+    c.bench_function("lmb read line", |b| {
+        b.iter_batched(
+            || {
+                let _ = e.rewind_input();
+            },
+            |_| e.evaluate().call().unwrap(),
+            BatchSize::SmallInput,
+        );
     });
 }
 
-fn lmb_read_number(bencher: &mut Bencher) {
+fn lmb_read_number(c: &mut Criterion) {
     let input = Cursor::new("0");
     let script = "return io.read('*n')";
     let e = Evaluation::builder(script, input).build().unwrap();
-    bencher.iter(|| {
-        let _ = e.rewind_input();
-        e.evaluate().call().unwrap()
+    c.bench_function("lmb read number", |b| {
+        b.iter_batched(
+            || {
+                let _ = e.rewind_input();
+            },
+            |_| e.evaluate().call().unwrap(),
+            BatchSize::SmallInput,
+        );
     });
 }
 
-fn lmb_read_unicode(bencher: &mut Bencher) {
+fn lmb_read_unicode(c: &mut Criterion) {
     let input = Cursor::new("0");
     let script = "return require('@lmb'):read_unicode(1)";
     let e = Evaluation::builder(script, input).build().unwrap();
-    bencher.iter(|| {
-        let _ = e.rewind_input();
-        e.evaluate().call().unwrap()
+    c.bench_function("lmb read unicode", |b| {
+        b.iter_batched(
+            || {
+                let _ = e.rewind_input();
+            },
+            |_| e.evaluate().call().unwrap(),
+            BatchSize::SmallInput,
+        );
     });
 }
 
-fn read_from_buf_reader(bencher: &mut Bencher) {
+fn read_from_buf_reader(c: &mut Criterion) {
     let mut r = BufReader::new(Cursor::new("1"));
-    bencher.iter(|| {
-        let mut buf = vec![0; 1];
-        let _ = r.read(&mut buf);
+    c.bench_function("read from buf reader", |b| {
+        b.iter_batched_ref(
+            || vec![0; 1],
+            |mut buf| {
+                let _ = r.read(&mut buf);
+            },
+            BatchSize::SmallInput,
+        );
     });
 }
 
-benchmark_group!(
+criterion_group!(
     evaluation,
     mlua_call,
     mlua_eval,
+    mlua_sandbox_call,
     mlua_sandbox_eval,
     lmb_evaluate,
 );
-benchmark_group!(
+criterion_group!(
     read,
     read_from_buf_reader,
     lmb_read_all,
@@ -136,11 +187,11 @@ benchmark_group!(
     lmb_read_number,
     lmb_read_unicode,
 );
-benchmark_group!(
+criterion_group!(
     store,
     store_update,
     lmb_no_store,
     lmb_default_store,
     lmb_update,
 );
-benchmark_main!(evaluation, read, store);
+criterion_main!(evaluation, read, store);
