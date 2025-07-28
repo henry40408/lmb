764a08c6ebb03d006ed4cbb4ab9eb9cee3056cb0
Heng-Yi Wu <2316687+henry40408@users.noreply.github.com>
2025-07-28 21:20:26 +0800

    style: fix indentation and ensure newline at end of file in store.lua and store-update.lua

--- benches/fixtures/store.lua
+++ benches/fixtures/store.lua
@@ -1,6 +1,6 @@
 function store_get(ctx)
-    ctx.store.a = true
-    assert(ctx.store.a == true, "ctx.store.a should be true")
+  ctx.store.a = true
+  assert(ctx.store.a == true, "ctx.store.a should be true")
 end
 
 return store_get
