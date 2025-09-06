8e97edb99500729250eaa415597a7807b9bf878e
Heng-Yi Wu <2316687+henry40408@users.noreply.github.com>
2025-09-06 11:13:17 +0800

    feat: add Lua error handling tests for value and expression errors

--- tests/error_integration_test.rs
+++ tests/error_integration_test.rs
@@ -25,3 +25,54 @@ Error: Lua error: runtime error: src/fixtures/errors/error.lua:2: unknown error
 
 "#]]);
 }
+
+#[test]
+fn eval_error_expr() {
+    Command::new(cargo_bin("lmb"))
+        .env("NO_COLOR", "true")
+        .args(["eval", "--file", "src/fixtures/errors/error-expr.lua"])
+        .assert()
+        .failure()
+        .stdout_eq(str![])
+        .stderr_eq(str![[r#"
+  x unknown error
+   ,-[@src/fixtures/errors/error-expr.lua:1:1]
+ 1 | error("unknown error")
+   : ^^^^^^^^^^^|^^^^^^^^^^^
+   :            `-- unknown error
+ 2 | return true
+   `----
+Error: Lua error: runtime error: src/fixtures/errors/error-expr.lua:1: unknown error
+
+"#]]);
+}
+
+#[test]
+fn eval_error_value() {
+    Command::new(cargo_bin("lmb"))
+        .env("NO_COLOR", "true")
+        .args(["eval", "--file", "src/fixtures/errors/error-value.lua"])
+        .assert()
+        .failure()
+        .stdout_eq(str![])
+        .stderr_eq(str![[r#"
+Lua value as error: {"a":1}
+Error: Lua value as error: {"a":1}
+
+"#]]);
+}
+
+#[test]
+fn eval_error_value_expr() {
+    Command::new(cargo_bin("lmb"))
+        .env("NO_COLOR", "true")
+        .args(["eval", "--file", "src/fixtures/errors/error-value-expr.lua"])
+        .assert()
+        .failure()
+        .stdout_eq(str![])
+        .stderr_eq(str![[r#"
+Lua value as error: {"a":1}
+Error: Lua value as error: {"a":1}
+
+"#]]);
+}
